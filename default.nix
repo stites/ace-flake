27b7d9c8e7c2ad7a45269afc629894b499d1a087
Sam Stites <stites@users.noreply.github.com>
2022-09-04 22:00:24 -0400

    make download error more direct

--- default.nix
+++ default.nix
@@ -11,12 +11,18 @@ stdenv.mkDerivation rec {
     (requireFile rec {
       name = "${srcs-package}.tar.gz";
       sha256 = "62ed2985177f2bd4c702c6f7db48a5926c69580a1facea2ca2b943ab9a4a731c";
-      message = "Download and run $ nix-store --add-fixed sha256 ~/Downloads/${name}";
+      message = ''
+        Download ACE from: http://reasoning.cs.ucla.edu/ace/download.php
+        then run $ nix-store --add-fixed sha256 ~/Downloads/${name}
+      '';
     })
     (requireFile rec {
       name = "${srcs-benchmarks}.zip";
       sha256 = "a699767ece03e0bb04065535210bd7f14d39aa842c1387a7ecb52d4967b2bf26";
-      message = "Download and run $ nix-store --add-fixed sha256 ~/Downloads/${name}";
+      message = ''
+        Download benchmarks from: http://reasoning.cs.ucla.edu/ace/download.php
+        then run $ nix-store --add-fixed sha256 ~/Downloads/${name}
+      '';
     })
   ];
 
